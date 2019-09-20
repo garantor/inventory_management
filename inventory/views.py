c72bf44d30f149aeefd713e4a2161935e38f0e73
afolabi <afolabisunday31@yahoo.com>
2019-09-20 21:11:49 +0100

    Fully functioning Inventory Management system that import and export data from excel or csv

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from.forms import *
 # Create your views here.
@@ -39,8 +39,6 @@ def add_device(request, cls):
             form.save()
             return redirect('index')
 
-
-
     else:
         form = cls
         return render(request, 'add_new.html', {'form':form})
@@ -56,3 +54,61 @@ def add_laptop(request):
 
 def add_mobile(request):
     return add_device(request, MobileForm)
+
+
+def edit_device(request, pk, model, cls):
+    item = get_object_or_404(model, pk=pk)
+
+    if request.method == 'POST':
+        form = cls(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = cls(instance=item)
+        return render(request, 'edit_item.html', {'form':form})
+
+def edit_laptop(request, pk):
+    return edit_device(request, pk, Laptop, LaptopForm)
+
+
+def edit_desktop(request, pk):
+    return edit_device(request, pk, Desktop, DesktopForm)
+
+def edit_mobile(request, pk):
+    return edit_device(request, pk, Mobile, MobileForm)
+
+
+
+def delete_laptop(request, pk):
+    Laptop.objects.filter(id=pk).delete()
+    items = Laptop.objects.all()
+
+    context = {
+        'items' : items
+    }
+
+    return render(request, 'index.html', context)
+
+
+def delete_desktop(request, pk):
+    Desktop.objects.filter(id=pk).delete()
+    items = Desktop.objects.all()
+
+    context = {
+        'items' : items
+    }
+
+    return render(request, 'index.html', context)
+
+
+def delete_mobile(request, pk):
+    Mobile.objects.filter(id=pk).delete()
+    items = Mobile.objects.all()
+
+    context = {
+        'items' : items
+    }
+
+    return render(request, 'index.html', context)
